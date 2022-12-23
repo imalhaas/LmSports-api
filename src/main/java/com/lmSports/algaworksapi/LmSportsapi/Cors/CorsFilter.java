6f4b55c12ecc357ca8852032eb1cf6fcf2cbd3ad
imalhaas <lucasdecassia9@hotmail.com>
2022-12-23 19:16:51 -0300

    .

---
+++ src/main/java/com/lmSports/algaworksapi/LmSportsapi/Cors/CorsFilter.java
@@ -0,0 +1,34 @@
+package com.lmSports.algaworksapi.LmSportsapi.Cors;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
